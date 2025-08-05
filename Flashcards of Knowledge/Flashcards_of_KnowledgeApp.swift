c5a0045a8e98fe46baee3ec5cbf1317f0ea9221e
Geraldy Martin <geraldymp24@gmail.com>
2025-08-05 14:11:00 +0700

    Initial Commit

---
+++ Flashcards of Knowledge/Flashcards_of_KnowledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcards_of_KnowledgeApp.swift
+//  Flashcards of Knowledge
+//
+//  Created by Geraldy on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Flashcards_of_KnowledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
